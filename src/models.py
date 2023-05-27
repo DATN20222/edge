e694efa904429d7a6fb71224d1f4a964d692e4ab
fioresxcat <e97520a@gmail.com>
2023-05-27 14:09:23 +0700

    fix

--- src/models.py
+++ src/models.py
@@ -95,7 +95,7 @@ class BodyFeatureExtractBackend():
 
     def warmup(self):
         LOGGER.info('Warming up extracting model...')
-        for i in range(10):
+        for i in range(20):
             im_tmp = np.random.randint(0, 255, (np.random.randint(20, 400), np.random.randint(20, 400), 3), "uint8")
             self.extract(im_tmp)
 
