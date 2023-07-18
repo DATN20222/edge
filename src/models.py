dad1fb9b342337b22c7798099b7c1348bc0f0324
Hieu Pham Trung <78094570+PhmHiu@users.noreply.github.com>
2023-07-18 20:10:26 +0700

    Fix warm up for body feature extraction model

--- src/models.py
+++ src/models.py
@@ -107,6 +107,7 @@ class BodyFeatureExtractBackend():
         LOGGER.info('Warming up extracting model...')
         for i in range(10):
             im_tmp = np.random.randint(0, 255, (np.random.randint(20, 400), np.random.randint(20, 400), 3), "uint8")
+            self.extract(im_tmp)
             
 
     def preprocess(self, img):
