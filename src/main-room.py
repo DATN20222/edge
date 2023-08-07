de172f2595aa301b49225f9de8dcedb7a0ce18bb
fiores <phamhieu412001@gmail.com>
2023-08-07 16:24:06 +0700

    fix ident

--- src/main-room.py
+++ src/main-room.py
@@ -20,32 +20,32 @@ import json
 from threading import Thread
 
 class VideoStream:
-	def __init__(self, src=0):
-		# initialize the video camera stream and read the first frame
-		# from the stream
-		self.stream = cv2.VideoCapture(src)
-		(self.grabbed, self.frame) = self.stream.read()
-		# initialize the variable used to indicate if the thread should
-		# be stopped
-		self.stopped = False
-	def start(self):
-		# start the thread to read frames from the video stream
-		Thread(target=self.update, args=()).start()
-		return self
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		while True:
-			# if the thread indicator variable is set, stop the thread
-			if self.stopped:
-				return
-			# otherwise, read the next frame from the stream
-			(self.grabbed, self.frame) = self.stream.read()
-	def read(self):
-		# return the frame most recently read
-		return self.grabbed, self.frame
-	def stop(self):
-		# indicate that the thread should be stopped
-		self.stopped = True
+    def __init__(self, src=0):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        (self.grabbed, self.frame) = self.stream.read()
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                return
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+    def read(self):
+        # return the frame most recently read
+        return self.grabbed, self.frame
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
     def isOpened(self):
         return self.stream.isOpened()
 
